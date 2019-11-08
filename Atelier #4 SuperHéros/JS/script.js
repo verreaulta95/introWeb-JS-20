855acc277099351e04264b5382a94431485d8f59
veral1533317 <veral1533317@etu.cegepjonquiere.ca>
2019-11-08 11:31:14 -0500

    Atelier 4 = a retravailler

--- Atelier #4 SuperHéros/JS/script.js
+++ Atelier #4 SuperHéros/JS/script.js
@@ -1,5 +1,33 @@
+var imageHero;
+imageHero = 0;
 function Hero_onclick(NoImage)
 {
   document.querySelector("#imgHero").src = "images/super-hero"+NoImage+".jpg" ;
+  imageHero = document.querySelector("#imgHero").src = "images/super-hero"+NoImage+".jpg" ;
+}
+
+function hero_onclickDisable()
+{
+
+  for(var i =0; i < 4; i++)
+  {
+    document.querySelector("#imgHero").disabled = true;
+  }
+
+}
+
+function hero_onclickActivated()
+{
+
+    for(var i =1; i < 4; i++)
+    {
+        document.querySelector("#imgHero").src = "images/super-hero"+imageHero+".jpg" ;
+        document.querySelector("#imgHero").disabled = false;
+    }
+}
+
+function hero_onclickImagePareil()
+{
+  alert("ok");
 
 }

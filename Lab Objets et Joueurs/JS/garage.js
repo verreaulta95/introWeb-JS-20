1fd2a39a84a2f2120ad941cdac3e14c1c0721d99
veral1533317 <veral1533317@etu.cegepjonquiere.ca>
2019-11-21 20:00:27 -0500

    Lab joueur pas tellement terminer

--- Lab Objets et Joueurs/JS/garage.js
+++ Lab Objets et Joueurs/JS/garage.js
@@ -1,6 +1,42 @@
+
+
+function Voiture(Marque,Model,AnneeConstruction)
+{
+    this.Marque = Marque;
+    this.Model = Model;
+    this.AnneeConstruction = AnneeConstruction;
+}
+
+
+
+function MoteurAutomobile(Marque,Force)
+{
+    this.Marque = Marque;
+    this.Force = Force;
+}
+
+
+
+function Garage(Nom,SommeArgent)
+{
+    this.Nom = Nom;
+    this.SommeArgent = SommeArgent;
+}
+
 function ExerciceGarage_onclick()
 {
+    var Voiture1 = new Voiture("Honda","Civic","2019");
+    var Voiture2 = new Voiture("Toyota","RAV4","2017");
+    var Voiture3 = new Voiture("GMC","Sierra","1998");
 
+    var MoteurAutomobile1 = new MoteurAutomobile("VTEC","180");
+    var MoteurAutomobile2 = new MoteurAutomobile("Normal","90");
+    var MoteurAutomobile3 = new MoteurAutomobile("Special","150");
+
+    var Garage1 = new Garage("Chez Alex",5000);
+    var Garage2 = new Garage("Chez Marc",2500);
+
+    document.querySelector("#lblRes1").innerHTML = Garage1.SommeArgent;
 }
 
 
--- Lab Objets et Joueurs/JS/script.js
+++ Lab Objets et Joueurs/JS/script.js
@@ -1,27 +1,87 @@
+var tabJoueur = new Array(5);
+var cpt =0;
 
-function btnAjouter_onclick()
+function Joueur(Prenom,Nom,Point)
 {
-	
+    this.Prenom = Prenom;
+    this.Nom = Nom;
+    this.Point = Point;
 }
 
 function ajouterJoueur()
 {
+    var nom = document.querySelector("#txtNom").value;
+    var age = document.querySelector("#txtAge").value;
+    var point = document.querySelector("#txtPoints").value;
+    tabJoueur[cpt] = new Joueur(nom,age,point);
+
+    for(var i =0; i < tabJoueur.length; i++ )
+    {
+        tabJoueur[i] = Joueur;
+    }
 
 }
 
-function gererBoutons(Active)
+function btnAjouter_onclick()
 {
+    ajouterJoueur();
+    cpt++;
 
+    if(cpt >= 5)
+    {
+        document.querySelector("#btnTrouverMoy").disabled = true;
+        document.querySelector("#btnTrouverMeilleur").disabled = true;
+    }
 }
+
+
 
 function btnTrouverMoy_onclick()
 {
-	
+    var total =0;
 
+	for(var i =0; i < tabJoueur.length; i++)
+    {
+        total = tabJoueur[i].Point + total;
+    }
+    total = total /5;
+    document.querySelector("#lblRes1").innerHTML = total;
 }
 
 function btnTrouverMeilleur_onclick()
 {
-	
+    var max = 1;
+    var joueurPlusGrand = "";
+    for(var i =0; i < tabJoueur.length; i++)
+    {
+        if(max < tabJoueur.Point)
+        {
+            max = tabJoueur[i].Point;
+            joueurPlusGrand = tabJoueur[i].Nom;
+        }
+
+        if( i > tabJoueur.length)
+        {
+            document.querySelector("#lblReponse").innerHTML = "Le joueur ayant le plus de point : "+ max + " est " + joueurPlusGrand;
+        }
+    }
+
 }
 
+
+function btnRechercher_onclick()
+{
+    var cptJoueur = 0;
+    while(cptJoueur < 5)
+    {
+        if(document.querySelector("#txtNom").value === tabJoueur[i].Nom)
+        {
+            document.querySelector("#txtPoints").innerHTML = tabJoueur[i].Point;
+            //mettre age mais je trouve pas (peut etre mon constructeur mal concu)
+        }
+        else
+        {
+            cptJoueur++;
+        }
+    }
+}

5381f3bb31964bf28668b890d4174de382d1e7cd
veral1533317 <veral1533317@etu.cegepjonquiere.ca>
2019-11-25 19:03:20 -0500

    Lab Regex terminer

--- Exercices expressions régulières/JS/script.js
+++ Exercices expressions régulières/JS/script.js
@@ -2,7 +2,7 @@
 //----------------------------------------------------------------------------------//
 function btnValider_onclick()
 {   var message;
-	if (valideAnnee(document.querySelector("#txtValidation").value ) === true) 
+	if (validerTel(document.querySelector("#txtValidation").value ) === true)
 	   message ="bon";
 	else
 	   message ="pas bon";
@@ -16,31 +16,31 @@ function valideAnnee(Chaine) // représente une année à quatre chiffres
 
 function validerNo( Chaine ) // un chiffre peut etre 0 5 25 200 etc..
 {
-	
+	return /^\d*$/.test(Chaine);
 }
 
 function validerNAS( Chaine ) // numéro d'assurance social 418 789 123, tester votre NAS
 {
-	
+    return /^\d{3} \d{3} \d{3}$/.test(Chaine);
 }
 
 function validerTel( Chaine )  // au format (418) 876-8373 , tester votre numéro de téléphone
 {
-	
+	return /^\(\d{3}\) \d{3} \d{4}$/.test(Chaine);
 }
 
 
 function verifierEntreDixTrente( Chaine )//un chiffre entre 10 et 30
 {
-	
+	return /^[1-2][0-9]|[3][0]$/.test(Chaine);
 }
 
 function verifierEntreUnVingt( Chaine )//un chiffre entre 1 et 20
 {
-	
+    return /^[1][0-9]|[2][0]$/.test(Chaine);
 }
 
 function validerCar( Chaine ) // des chaînes de caractères alphabétiques majuscule et minuscule et accepte le trait d'union pas obligatoire
 {
-	
+    return /^[A-z]*-*[A-z]*$/.test(Chaine);
 }
